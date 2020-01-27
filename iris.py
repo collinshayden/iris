62972d3da936c21d3983baf66e51d86a43ce8036
collinshayden <48135191+collinshayden@users.noreply.github.com>
2020-01-27 11:41:17 -0500

    added code
    with predictions made and evaluated. Working on adding code to represent the data with graphs.

--- iris.py
+++ iris.py
@@ -14,6 +14,8 @@ class Iris(object):
         self.input_nodes = input_nodes
         self.output_nodes = output_nodes
         self.dataArray = dataArray
+
+        #empty arrays
         self.inputData = []
         self.targets = []
 
@@ -23,20 +25,50 @@ class Iris(object):
         self.testData = []
         self.testTargets = []
 
+        self.sepal_length = []
+        self.sepal_width = []
+        self.petal_length = []
+        self.petal_width = []
+
+        self.KNC = KNeighborsClassifier(n_neighbors=4)
+
     def Data(self):#organizes data
         for i in self.dataArray:
             self.inputData.append([i[0],i[1],i[2],i[3]])#creates inputData array with only features
             self.targets.append(i[4])#creates targets array with only classifications
-        self.trainData.append([self.inputData[0:40],self.inputData[50:90],self.inputData[100:140]])#80% of data
-        self.trainTargets.append([self.targets[0:40],self.targets[50:90],self.targets[100:140]])#corresponding 80% of classifications
 
-        self.testData.append([self.inputData[40:50],self.inputData[90:100],self.inputData[140:150]])#20% of data
-        self.testTargets.append([self.targets[40:50],self.targets[90:100],self.targets[140:150]])#corresponding 20% of classifications
+        self.trainData = np.concatenate((self.inputData[0:40],self.inputData[50:90],self.inputData[100:140]))#80% of data
+        self.trainTargets = np.concatenate((self.targets[0:40],self.targets[50:90],self.targets[100:140]))#corresponding 80% of classifications
 
+        self.testData = np.concatenate((self.inputData[40:50],self.inputData[90:100],self.inputData[140:150]))#20% of data
+        self.testTargets = np.concatenate((self.targets[40:50],self.targets[90:100],self.targets[140:150]))#corresponding 20% of classifications
+        
+
+    def Train(self):
+        #KNC.fit(X,y) where X is training data and y is targets. This function is training the algorithm to 80% of the dataset.
+        self.KNC.fit(self.trainData,self.trainTargets)
+
+    def Predict(self):
+        #KNC.predict(X) where X is unlabelled data, takes the data points and compares them to nearby labelled data to identify a classification
+        self.prediction = self.KNC.predict(self.testData)
+
+    def Scoring(self):
+        self.score=0
+        for i in range(0,len(self.prediction),1):
+            if(self.prediction[i] == self.testTargets[i]):#if the predictions and targets are the same:
+                self.score += 1#increase score by 1
+
+        #printing values
+        print("Predictions Correct = "+str(self.score))
+        print("Predictions Made = "+str(len(self.prediction)))
+
+        self.Accuracy = (self.score/len(self.testData))*100
+        self.percentAccuracy=(str(self.Accuracy)+"%")
+
+        print("Percent Accuracy = "+str(self.percentAccuracy))
 
 iris = Iris(4,3,dataArray)
 Iris.Data(iris)
-
-
-
-
+Iris.Train(iris)
+Iris.Predict(iris)
+Iris.Scoring(iris)
